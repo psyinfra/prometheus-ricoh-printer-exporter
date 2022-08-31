13bbaf992eb1d16d075388367697a72c35f87636
odruska <oskar.druska@alumni.fh-aachen.de>
2022-08-31 10:50:30 +0200

    shortened crawled printer data evaluation makes use of new scrape_printers() function to directly scrape printers from given URL

--- prometheus_ricoh_printer_exporter/exporter_file.py
+++ prometheus_ricoh_printer_exporter/exporter_file.py
@@ -7,52 +7,29 @@
 from prometheus_client import Summary
 from prometheus_client.core import GaugeMetricFamily
 
-from . import ricoh_data_crawler as ricoh
+from .data import scrape_printers
 
 REQUEST_TIME = Summary("ricoh_printer_exporter_collect_",
                        "Time spent to collect metrics from ricoh_data_crawler.py")
 
 
 class RicohPrinterExporter:
-    soups: list
-    urls: list
-
-    def __init__(self, soups, urls) -> None:
-        self.soups = soups
-        self.urls = urls
+    def __init__(self, printers, insec: bool) -> None:
+        self.printers = printers
+        self.insecure = insec
 
     @REQUEST_TIME.time()
     def collect(self):
+        # returns a Printer_Values object in each generator call
+        printers = scrape_printers(printers=self.printers, insecure=self.insecure)
 
-        printer_generator = ricoh.get_printer_values(self.soups, self.urls)  # returns a Printer_Values object in each generator call
-
-        for printer in printer_generator:
-            labels = ['printer_name', 'color']
-
+        for printer in printers:
             g = GaugeMetricFamily(
                 name='ricoh_printer_level_percent',
-                labels=labels,
-                documentation='black toner level in percent')
-            g.add_metric([printer.printer_name, 'black'], printer.level_black)
-            yield g
-
-            g = GaugeMetricFamily(
-                name='ricoh_printer_level_percent',
-                labels=labels,
-                documentation='cyan toner level in percent')
-            g.add_metric([printer.printer_name, 'cyan'], printer.level_cyan)
-            yield g
-
-            g = GaugeMetricFamily(
-                name='ricoh_printer_level_percent',
-                labels=labels,
-                documentation='magenta toner level in percent')
-            g.add_metric([printer.printer_name, 'magenta'], printer.level_magenta)
-            yield g
-
-            g = GaugeMetricFamily(
-                name='ricoh_printer_level_percent',
-                labels=labels,
-                documentation='yellow toner level in percent')
-            g.add_metric([printer.printer_name, 'yellow'], printer.level_yellow)
+                labels=['name', 'color'],
+                documentation='toner level in percent')
+            g.add_metric([printer.name, 'black'], printer.level_black)
+            g.add_metric([printer.name, 'cyan'], printer.level_cyan)
+            g.add_metric([printer.name, 'magenta'], printer.level_magenta)
+            g.add_metric([printer.name, 'yellow'], printer.level_yellow)
             yield g
